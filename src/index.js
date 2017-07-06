eb064eea2a604d02ecfb55d17c385cea4a8642fb
Luke Jackson <lukejacksonn@gmail.com>
2017-07-06 03:33:02 +0100

    Tidies up view code

--- src/index.js
+++ src/index.js
@@ -2,28 +2,13 @@ const { h, app } = require('hyperapp')
 const devtools = require('hyperapp-redux-devtools')
 const persist = require('hyperapp-hmr')
 
-const $title = h('h1', {}, 'hyperapp-electron')
-const $subtitle = h('h3', {}, 'All of the Node.js APIs are available in this file.')
-const $list = children => h('ul', {}, children)
-const $item = children => h('li', {}, children)
-const $span = children => h('span', {}, children)
-const $button = (props, children) => h('button', props, children)
-
-const $main = (s,a) =>
-  h('main', {}, [
-    $title,
-    $subtitle,
-    $list([
-      $item(`Node.js ${process.versions.node}`),
-      $item(`Chromium ${process.versions.chrome}`),
-      $item(`Electron ${process.versions.electron}.`),
-    ]),
-    h('counter-', {}, [
-      $button({ onclick: a.inc }, 'INC'),
-      $button({ onclick: a.dec }, 'DEC'),
-      $span(s.count),
-    ])
-  ])
+const $title = c => h('h1', {}, c)
+const $subtitle = c => h('h3', {}, c)
+const $list = c => h('ul', {}, c)
+const $item = c => h('li', {}, c)
+const $span = c => h('span', {}, c)
+const $counter = c => h('counter-', {}, c)
+const $button = (p, c) => h('button', p, c)
 
 app({
   state: {
@@ -33,7 +18,21 @@ app({
     inc: (s,a,d) => ({ count: s.count + 1 }),
     dec: (s,a,d) => ({ count: s.count - 1 }),
   },
-  view: (s,a) => $main(s,a),
+  view: (s,a) =>
+    h('main', {}, [
+      $title('hyperapp-electron'),
+      $subtitle('All of the Node.js APIs are available in this file.'),
+      $list([
+        $item(`Node.js ${process.versions.node}`),
+        $item(`Chromium ${process.versions.chrome}`),
+        $item(`Electron ${process.versions.electron}.`),
+      ]),
+      $counter([
+        $button({ onclick: a.inc }, 'INC'),
+        $button({ onclick: a.dec }, 'DEC'),
+        $span(s.count),
+      ])
+    ]),
   mixins: [
     devtools(),
     persist,
